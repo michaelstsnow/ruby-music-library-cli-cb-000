51d0fa5a5dc68bc0ae77aa728d1945bec12ed164
michaelstsnow <michael.st.snow@gmail.com>
2019-01-22 04:39:22 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -13,9 +13,7 @@ class MusicImporter
 
   def import
     music_files=self.files
-    song=music_files.each do { |song_info|
-      Song.create_from_filename(song_info)
-    }
+    music_files.each do { |song_info| Song.create_from_filename(song_info) }
   end
 
 end
