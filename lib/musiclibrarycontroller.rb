71933aa4174b89cb7bf8ae2fe423e70068103a16
michaelstsnow <michael.st.snow@gmail.com>
2019-01-22 06:01:49 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -23,10 +23,15 @@ class MusicLibraryController
 
       case user_input
       when "list songs"
+        self.list_songs
       when "list artists"
+        self.list_artists
       when "list genres"
+        self.list_genres
       when "list artist"
+        self.list_songs_by_artist
       when "list genre"
+        self.list_songs_by_genre
       when "play song"
       end
 
